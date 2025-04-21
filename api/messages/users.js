ec69d63f0757bdff89d0fa6bad032aede14ea531
AzulaFire <jhornjr@gmail.com>
2025-04-21 16:44:45 +0900

    Updated backend

--- api/messages/users.js
+++ api/messages/users.js
@@ -1,5 +1,6 @@
 import { connectDb } from '../../lib/connectDb.js';
 import User from '../../models/user.model.js';
+import { verifyToken } from '../../lib/utils.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET')
@@ -8,10 +9,12 @@ export default async function handler(req, res) {
   try {
     await connectDb();
 
-    const userId = req.headers['x-user-id'];
-    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+    const user = await verifyToken(req); // 👈 Extract user from cookie
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
 
-    const users = await User.find({ _id: { $ne: userId } }).select('-password');
+    const users = await User.find({ _id: { $ne: user._id } }).select(
+      '-password'
+    );
     return res.status(200).json(users);
   } catch (err) {
     console.error('Error in users handler:', err);
